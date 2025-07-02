eeb63583dda154e63e0541d3bbad33d34666fc9b
Amanda Sousa <silvasousaamandapaula@gmail.com>
2025-07-02 08:07:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Script para mostrar o botão "Voltar ao Topo"
+
 const backToTopButton = document.getElementById('backToTop');
 window.onscroll = function() {
     if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
@@ -8,15 +8,15 @@ window.onscroll = function() {
     }
 };
 
-// Efeito de aparecimento suave
+
 document.addEventListener('DOMContentLoaded', () => {
     // Seleciona todos os elementos com data-anime
     const elementos = document.querySelectorAll('[data-anime]');
     
-    // Configurações do efeito
+ 
     const windowHeight = window.innerHeight * 0.85;
     
-    // Função do efeito
+
     const animarScroll = () => {
         elementos.forEach((item) => {
             const itemTop = item.getBoundingClientRect().top;
@@ -28,14 +28,13 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     };
     
-    // Ativa na carga inicial
+   
     animarScroll();
     
-    // Ativa no scroll
+ 
     window.addEventListener('scroll', animarScroll);
 });
 
-// Adiciona data-anime AUTOMATICAMENTE em todos os elementos visíveis
 window.onload = function() {
     const allElements = document.querySelectorAll('body *');
     allElements.forEach(el => {
